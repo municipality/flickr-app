bb3a81c52a6c65a8fb7f1283fb942f39fff890ab
municipality <Municipality@Brians-Laundry.local>
2016-09-12 22:06:17 -0700

    header component css

--- app/components/header.component.ts
+++ app/components/header.component.ts
@@ -8,11 +8,12 @@ import {HeaderRoutes} from '../app.routing';
             <h1>Brianium</h1>
         </div>
         <div class="navbar">
-            <div class="icon"
+            <div tabindex=0 class="link"
                  *ngFor="let route of routeData; let i = index"
                  routerLink="{{route.path}}" routerLinkedActive="active">
-                 <div class="icon-sym {{route.iconCls}}">{{route.name}}</div>
-                 <p *ngIf="i != 0">{{route.display}}</p>
+                <!-- <div class="icon-sym {{route.iconCls}}"> -->
+                 <p>{{route.name}}</p>
+                 <!-- </div> -->
             </div>
         </div>
     `
